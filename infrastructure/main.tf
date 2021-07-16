c105a81c8c2bd45e13acef457c9974dd23d96308
Steve Bowser <stevewbowser@gmail.com>
2021-07-16 14:45:16 +0100

    setting up infrastructure for cloud functions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,7 +16,37 @@ resource "google_project_service" "ai-platform-api" {
   service = "ml.googleapis.com"
 }
 
+resource "google_project_service" "cloud-functions-api" {
+  project = var.project_id
+  service = "cloudfunctions.googleapis.com"
+}
+
+resource "google_project_service" "cloud-build-api" {
+  project = var.project_id
+  service = "cloudbuild.googleapis.com"
+}
+
+
+
+
 resource "google_ml_engine_model" "gdelt_ftse_regression_model" {
   name        = "gdelt_ftse_regression_model"
   regions     = ["europe-west1"]
 }
+
+resource "google_storage_bucket_object" "ml-model-file" {
+  name   = "model.joblib"
+  bucket = google_storage_bucket.model-storage.name
+  source = "../model_generation/model.joblib"
+}
+
+# resource "google_cloudfunctions_function" "function" {
+#   name        = "function-test"
+#   description = "My function"
+#   runtime     = "python39"
+# 
+#   available_memory_mb   = 128
+#   trigger_http          = true
+#   timeout               = 60
+#   
+# }
